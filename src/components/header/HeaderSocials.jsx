f4d53d0356ea70ebadd5bca9ecedfc600129b66e
Dipak <dipakgiri1200@gmail.com>
2022-12-22 21:15:20 +0545

    social updated

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 import { BsLinkedin } from "react-icons/bs";
 import { FaGithub } from "react-icons/fa";
-import { FiDribbble } from "react-icons/fi";
+import { FiDribbble, FiFacebook } from "react-icons/fi";
 
 const HeaderSocials = () => {
   return (
     <div className="header__socials">
-      <a href="https://www.linkedin.com/in/manohar19" target="_blank">
+      <a href="https://www.linkedin.com/in/dpakgiri" target="_blank">
         <BsLinkedin />
       </a>
-      <a href="https://github.com/manohar-19" target="_blank">
+      <a href="https://github.com/dipaktech" target="_blank">
         <FaGithub />
       </a>
-      <a href="https://dribbble.com" target="_blank">
-        <FiDribbble />
+      <a href="https://facebook.com/Dpakgirii" target="_blank">
+        <FiFacebook />
       </a>
     </div>
   );
